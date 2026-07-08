34ff763650694bcea17cab0d5f4c25744a4f468d
25ds1000094-cmd <25ds1000094@ds.study.iitm.ac.in>
2026-07-08 19:13:33 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI
+import redis
+import os
+
+app = FastAPI()
+
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+
+r = redis.Redis(
+    host=REDIS_HOST,
+    port=6379,
+    decode_responses=True
+)
+
+
+@app.post("/hit/{key}")
+def hit(key: str):
+
+    count = r.incr(key)
+
+    return {
+        "key": key,
+        "count": count
+    }
+
+
+@app.get("/count/{key}")
+def count(key: str):
+
+    value = r.get(key)
+
+    return {
+        "key": key,
+        "count": int(value) if value else 0
+    }
+
+
+@app.get("/healthz")
+def health():
+
+    try:
+        r.ping()
+
+        return {
+            "status": "ok",
+            "redis": "up"
+        }
+
+    except Exception:
+
+        return {
+            "status": "error",
+            "redis": "down"
+        }
